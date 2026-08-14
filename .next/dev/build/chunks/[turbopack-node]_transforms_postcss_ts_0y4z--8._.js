12602ae074eeeb05b7c1033e45656e6ce1af464c
thiranprivateltd-cloud <thiranprivateltd@gmail.com>
2026-08-14 18:35:29 +0530

    Add files via upload

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_0y4z--8._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/.gemini/antigravity/scratch/thiran-corporate/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/0qhg_0zw8pu-._.js",
+  "chunks/[root-of-the-server]__0c2kdwb._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/.gemini/antigravity/scratch/thiran-corporate/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
